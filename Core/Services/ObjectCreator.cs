c9cbe6288987f23f50bc0bb7be9b5d82ea469722
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-10-17 21:19:37 +0300

    Add ObjectCreator and ObjectInitor

---
+++ Core/Services/ObjectCreator.cs
@@ -0,0 +1,36 @@
+﻿using Faker.Interfaces;
+
+namespace Faker.Core
+{
+    public class ObjectCreator
+    {
+        private readonly IFaker _faker;
+        public ObjectCreator(IFaker faker)
+        {
+            _faker = faker;
+        }
+
+        public object CreateObject(Type type)
+        {
+            var typeConstructors = type.GetConstructors()
+                .OrderByDescending(c => c.GetParameters().Length)
+                .ToList();
+
+            foreach (var ctor in typeConstructors)
+            {
+                try
+                {
+                    var parameters = ctor.GetParameters()
+                        .Select(t => t.ParameterType)
+                        .Select(_faker.Create).ToArray();
+
+                    return ctor.Invoke(parameters);
+                }
+                catch
+                { }
+            }
+
+            throw new Exception();
+        }
+    }
+}
---
+++ Core/Services/ObjectInitor.cs
@@ -0,0 +1,48 @@
+﻿using Faker.Interfaces;
+
+namespace Faker.Core
+{
+    public class ObjectInitor
+    {
+        private readonly IFaker _faker;
+        public ObjectInitor(IFaker faker)
+        {
+            _faker = faker;
+        }
+
+        public void InitFields(object obj, Type objectType)
+        {
+            var fields = objectType.GetFields().Where(f => !f.IsInitOnly);
+
+            foreach (var field in fields)
+            {
+                try
+                {
+                    if (Equals(field.GetValue(obj), GetDefaultValue(field.FieldType)))
+                        field.SetValue(obj, _faker.Create(field.FieldType));
+                }
+                catch { }
+            }
+        }
+
+        public void InitProps(object obj, Type objectType)
+        {
+            var props = objectType.GetProperties().Where(p => p.CanWrite);
+
+            foreach (var prop in props)
+            {
+                try
+                {
+                    if (Equals(prop.GetValue(obj), GetDefaultValue(prop.PropertyType)))
+                        prop.SetValue(obj, _faker.Create(prop.PropertyType));
+                }
+                catch { }
+            }
+        }
+
+        public static object? GetDefaultValue(Type type)
+        {
+            return type.IsValueType ? Activator.CreateInstance(type) : null;
+        }
+    }
+}
