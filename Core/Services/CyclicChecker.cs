8521442938a6373fa902153bd1c25e602085d60b
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-10-17 21:44:52 +0300

    Add CyclicChecker

---
+++ Core/Services/CyclicChecker.cs
@@ -0,0 +1,27 @@
+﻿namespace Faker.Core
+{
+    public class CyclicChecker
+    {
+        private Dictionary<Type, int> _nestingCounter = new ();
+        private static int maxNestingLevel = 3;
+
+        public bool IsCyclic(Type type)
+        {
+            if (_nestingCounter.ContainsKey(type))
+            {
+                _nestingCounter[type]++;
+            }
+            else
+            {
+                _nestingCounter.Add(type, 0);
+            }
+
+            return _nestingCounter[type] >= maxNestingLevel;
+        }
+
+        public void DeleteFromCycle(Type type)
+        {
+            _nestingCounter[type]--;
+        }
+    }
+}
